f3db7f2fb201ad94dc5173b5d7418b0def0a2915
Pedro Corrêa <pedrocorrea23dev@gmail.com>
2025-05-22 20:52:50 -0300

    JS

---
+++ js/script.js
@@ -0,0 +1,121 @@
+//Declarações
+const perguntas = [
+  {
+    pergunta:
+      "Qual é o seu meio de transporte mais utilizado durante a semana?",
+    alt1: "Carro sozinho",
+    alt2: "Carro com carona",
+    alt3: "Transporte público",
+    alt4: "Bicicleta ou caminhada",
+  },
+  {
+    pergunta: "Com que frequência você consome carne vermelha?",
+    alt1: "Diariamente",
+    alt2: "3 a 5 vezes por semana",
+    alt3: "1 a 2 vezes por semana",
+    alt4: "Raramente ou nunca",
+  },
+  {
+    pergunta: "Como é o consumo de energia elétrica na sua casa?",
+    alt1: "Alta",
+    alt2: "Média",
+    alt3: "Baixa",
+    alt4: "Muito baixa",
+  },
+  {
+    pergunta: "Qual é o seu nível de reciclagem de resíduos?",
+    alt1: "Não reciclo nada",
+    alt2: "Reciclo papel ou plástico",
+    alt3: "Reciclo quase tudo",
+    alt4: "Reciclo tudo corretamente",
+  },
+  {
+    pergunta: "Com que frequência você compra roupas novas?",
+    alt1: "Toda semana",
+    alt2: "Todo mês",
+    alt3: "A cada 2-3 meses",
+    alt4: "Raramente",
+  },
+];
+const imgs = [
+  "img/img-body/placeholder.svg",
+  "img/img-body/carne_vermelha.svg",
+  "img/img-body/energia.svg",
+  "img/img-body/recycle.svg",
+  "img/img-body/clothes.svg",
+];
+let respostas = [];
+let texto_principal = document.querySelector(".perguntas-p");
+let pergunta_1 = document.querySelector(".alternativa_1");
+let pergunta_2 = document.querySelector(".alternativa_2");
+let pergunta_3 = document.querySelector(".alternativa_3");
+let pergunta_4 = document.querySelector(".alternativa_4");
+let value1 = document.querySelector(".alternativa_value1");
+let value2 = document.querySelector(".alternativa_value2");
+let value3 = document.querySelector(".alternativa_value3");
+let value4 = document.querySelector(".alternativa_value4");
+let botao_troca = document.querySelector(".action_button");
+let radio_selecionado = document.querySelectorAll('input[name="escolha"]');
+let sliders = document.querySelector(".imagem");
+const botao_resultado = document.querySelector(".ver_resultado");
+const container_perguntas = document.querySelector(".sumir");
+cont = 1;
+//
+//Declarando Função
+function desmarcarRadios() {
+  const radios = document.querySelectorAll(".input");
+  radios.forEach((radio) => {
+    radio.checked = false;
+    radio.type = "checkbox";
+    radio.type = "radio";
+  });
+}
+function armazenar() {
+  radio_selecionado.forEach((radio) => {
+    if (radio.checked) {
+      respostas.push(radio.value);
+    }
+  });
+}
+function trocar() {
+  radio_selecionado.forEach((radio) => {
+    if (radio.checked) {
+      armazenar();
+      desmarcarRadios();
+
+      //ATRiBUIÇÕES
+      sliders.src = imgs[cont];
+      texto_principal.innerHTML = perguntas[cont].pergunta;
+      pergunta_1.innerHTML = perguntas[cont].alt1;
+      pergunta_2.innerHTML = perguntas[cont].alt2;
+      pergunta_3.innerHTML = perguntas[cont].alt3;
+      pergunta_4.innerHTML = perguntas[cont].alt4;
+      value1.value = perguntas[cont].alt1;
+      value2.value = perguntas[cont].alt2;
+      value3.value = perguntas[cont].alt3;
+      value4.value = perguntas[cont].alt4;
+      //
+      cont += 1;
+      if (cont === 5) {
+        botao_troca.style.display = "none";
+        botao_resultado.style.display = "initial";
+      }
+      console.log(cont);
+    }
+  });
+}
+function results() {
+  //Sumindo com o site
+  radio_selecionado.forEach((radio) => {
+    if (radio.checked) {
+      armazenar();
+      container_perguntas.style.display = "none";
+      sliders.style.display = "none";
+      botao_resultado.style.display = "none";
+      //
+    }
+  });
+}
+
+botao_troca.addEventListener("click", trocar);
+botao_resultado.addEventListener("click", results);
